ef4541568a72b424029eb0e43107f30eaf10dbd7
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2024-12-23 22:46:05 +0300

    StogServerApi read WorldState

---
+++ StogServerApi/Controllers/WorldStateController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StogShared.Entities.GameWorld;
+
+namespace StogServerApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WorldStateController : ControllerBase
+{
+    private readonly ILogger _logger;
+    private readonly WorldState _worldState;
+    
+    public WorldStateController(ILogger<WorldStateController> logger, WorldState worldState)
+    {
+        _logger = logger;
+        _worldState = worldState;
+    }
+
+    [HttpGet]
+    [Route("Read")]
+    [Authorize]
+    public async Task<IResult> Read()
+    {
+        return Results.Json(_worldState);
+    }
+}
