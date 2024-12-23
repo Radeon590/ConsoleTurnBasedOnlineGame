f2a1cac4d618ea20c4106619a80b26a50a66e1c6
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2024-12-23 22:45:40 +0300

    StogServerApi connection to launcher

---
+++ StogServerApi/LauncherConnector.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using StogShared.Entities;
+
+namespace StogServerApi;
+
+public class LauncherConnector : IHostedService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+    private readonly string _launcherApiUrl;
+    private readonly GameServerData _gameServerData;
+
+    public LauncherConnector(HttpClient client, ILogger<LauncherConnector> logger,
+        IOptions<GameServerData> gameServerData, IConfiguration configuration)
+    {
+        _httpClient = client;
+        _logger = logger;
+        
+        // TODO: bind obj from configuration
+        var section = configuration.GetSection("GameServerData");
+        _gameServerData = new GameServerData()
+        {
+            ServerName = section.GetValue<string>("ServerName"),
+            ServerConnectionString = section.GetValue<string>("ServerConnectionString"),
+        };
+            
+        _launcherApiUrl = configuration.GetValue<string>("LauncherApiUrl");
+        if (string.IsNullOrEmpty(_launcherApiUrl))
+        {
+            _logger.LogInformation("LauncherApiUrl is missing");
+        }
+    }
+
+    private async Task Connect()
+    {
+        _logger.LogInformation($"Connecting to Launcher {_launcherApiUrl} with GameServerData {JsonConvert.SerializeObject(_gameServerData)}");
+        var result = await _httpClient.PostAsJsonAsync($"{_launcherApiUrl}/GameServers/Connect", _gameServerData);
+        if (!result.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("Failed to connect to Launcher api: {ResultStatusCode}", result.StatusCode);
+        }
+        else
+        {
+            _logger.LogInformation("Successfully connected to Launcher api: {ResultStatusCode}", result.StatusCode);
+        }
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Connect();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        
+    }
+}
---
+++ StogServerApi/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using StogServerApi;
+using StogShared.Authorization;
+using StogShared.Entities;
+using StogShared.Entities.GameWorld;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<GameServerData>(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton(new HttpClient());
+builder.Services.AddSingleton(new WorldState());
+builder.Services.AddSingleton<IHostedService, LauncherConnector>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
